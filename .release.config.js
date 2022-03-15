956e612f3d25aa8315bd12ac5bde4d15900aec87
tlindener <tobias.lindener@gmail.com>
2022-03-15 12:41:55 +0100

    feat(table): initial version

---
+++ .release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    branches: [
+        'master',
+        {
+            name: 'beta',
+            prerelease: true
+        }
+    ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md'
+            }
+        ],
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git', {
+                assets: ['CHANGELOG.md', 'lib/**'],
+                message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ]
+    ]
+}
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios"
+import Papa, { ParseResult, ParseWorkerConfig } from 'papaparse'
+
+function parseCSV<T = any>(string: string, config: Omit<ParseWorkerConfig<T>, "complete" | "worker">) {
+    return new Promise<ParseResult<T>>(function (complete, error) {
+        Papa.parse<T>(string, { ...config, complete, error });
+    });
+};
+
+const baseUrl = process.env.BASE_URL ?? "https://www-genesis.destatis.de/genesisWS/rest"
+axios.interceptors.request.use((config) => {
+    config.baseURL = baseUrl
+    return config
+})
+
+export const fetchTable = async ({ format = "ffcsv", area = "all", ...rest }: { username: string, password: string, name: string, format?: string, area?: string }) => {
+    const response = await axios.get(`/2020/data/tablefile`, {
+        params: {
+            ...rest,
+            area,
+            format,
+            language: "de"
+        }
+    })
+    if (response.status === 200 && format === "ffcsv") {
+        const result = await parseCSV(response.data, { header: true })
+        return result.data        
+    }
+    return response.data
+}
